37a89eb408450f299027bf33c23fbda42164580f
Daniel Sabbagh <dsabbaghumd@gmail.com>
2015-04-03 12:16:37 -0400

    Added method to find all weeks for use in dropdown

--- src/main/java/app/WeekRepository.java
+++ src/main/java/app/WeekRepository.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.data.domain.*;
 import org.springframework.data.repository.*;
@@ -11,4 +12,6 @@ public interface WeekRepository extends Repository<Week, Integer> {
 	Page<Week> findAll(Pageable pageable);
 
 	Week findWeekByDate(String date);
+	
+	List<Week> findAll();
 }
