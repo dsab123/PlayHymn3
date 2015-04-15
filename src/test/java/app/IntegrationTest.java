2d0f20dd5889b523ef9b7d1380bd50f2c55ba916
dsab123 <dsabbaghumd@gmail.com>
2015-04-14 20:58:13 -0400

    added s3 sdk

--- src/test/java/app/IntegrationTest.java
+++ src/test/java/app/IntegrationTest.java
@@ -29,18 +29,10 @@ public class IntegrationTest {
 		//driver = new ChromeDriver();
 		driver = new FirefoxDriver();
 		driver.get("https://mysterious-mesa-5118.herokuapp.com");
-		
-		System.out.println("driver set up.");
 	}
 	
 	@Test
 	public void verifyPageOpens() {
-		
-		driver = new FirefoxDriver();
-		driver.get("https://mysterious-mesa-5118.herokuapp.com");
-		
-		System.out.println("driver set up.");
-		
 		assertThat(driver.getTitle(), is(equalTo("PlayHymns")));
 	}
 	
