a65b89f3e9fb0d074331bb5ea2286193c6e3ad54
Daniel Sabbagh <dsabbaghumd@gmail.com>
2015-04-21 22:33:29 -0400

    new properties file for test profile

--- src/main/java/app/SimpleCORSFilter.java
+++ src/main/java/app/SimpleCORSFilter.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Profile("test")
 public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
